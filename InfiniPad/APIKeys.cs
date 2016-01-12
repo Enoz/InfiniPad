3068ab51949b616d6553ce24950c682b8282f272
Enoz <hyper_iguana@yahoo.com>
2016-01-11 20:50:07 -0600

    Added about page

--- InfiniPad/APIKeys.cs
+++ InfiniPad/APIKeys.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InfiniPad
+﻿namespace InfiniPad
 {
     class APIKeys
     {
-        public static string ImgurClientID = "Insert Your API Key Here";
+        public static string ImgurClientID = "7f3299729ebd250";
     }
 }
