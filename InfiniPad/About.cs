f9d3d138537bf27dfe1f191bc842793e09168331
Enoz <hyper_iguana@yahoo.com>
2016-01-11 20:47:22 -0600

    Added about page

---
+++ InfiniPad/About.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace InfiniPad
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+            pictureIcon.Image = new Icon(Properties.Resources.icon, 256, 256).ToBitmap();
+            labelVersion.Text = "Version: " + Assembly.GetEntryAssembly().GetName().Version;
+        }
+
+        private void btnGitHub_Click(object sender, System.EventArgs e)
+        {
+            Process.Start("https://github.com/Enoz/InfiniPad");
+        }
+
+        private void About_Load(object sender, System.EventArgs e)
+        {
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+    }
+}
