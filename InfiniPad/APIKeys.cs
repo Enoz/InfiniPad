094af1a97c6694cd818b492aff4671ffd981b94c
Enoz <hyper_iguana@yahoo.com>
2016-01-11 20:13:47 -0600

    Update APIKeys.cs
    Added notice about API Key

--- InfiniPad/APIKeys.cs
+++ InfiniPad/APIKeys.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/*
+    Note: You must use your own API key. In the source code, the API key will not be included.
+    It is included in the build, however.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
