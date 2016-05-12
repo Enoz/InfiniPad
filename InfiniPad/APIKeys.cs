2f83170fc966a99ecfa1a927b4acda78b6c40bf0
Enoz <hyper_iguana@yahoo.com>
2016-05-11 20:33:09 -0600

    Nothing

--- InfiniPad/APIKeys.cs
+++ InfiniPad/APIKeys.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
     Note: You must use your own API key. In the source code, the API key will not be included.
     It is included in the build, however.
 */
@@ -7,6 +7,6 @@ namespace InfiniPad
 {
     class APIKeys
     {
-        public static string ImgurClientID = "Insert Your API Key Here";
+        public static string ImgurClientID = "7f3299729ebd250";
     }
 }
