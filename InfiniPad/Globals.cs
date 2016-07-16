60215ae2f3e6198d8e1927dfb37ba34f698d09e9
Enoz <hyper_iguana@yahoo.com>
2016-07-16 11:38:18 -0500

    "using" organization #2

--- InfiniPad/Globals.cs
+++ InfiniPad/Globals.cs
@@ -1,14 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Windows.Forms;
 using System.Drawing;
-using Microsoft.Win32;
+using System.IO;
 using System.Reflection;
-using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace InfiniPad
 {
